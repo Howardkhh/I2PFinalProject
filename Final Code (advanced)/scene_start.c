994876050918b365f391f6ca414a8001df2c4430
Howard Hou <howard.hou.fan@gmail.com>
2021-01-16 18:33:28 +0800

    added middle enemy

--- Final Code (advanced)/scene_start.c
+++ Final Code (advanced)/scene_start.c
@@ -7,13 +7,16 @@
 #include "scene_menu.h"
 #include <math.h>
 
+#define PI 3.14159265358979323846
 
 static ALLEGRO_BITMAP* img_background;
 static ALLEGRO_BITMAP* img_plane;
 static ALLEGRO_BITMAP* img_small_fighter;
 static ALLEGRO_BITMAP* img_small_fighter_broken1;
 static ALLEGRO_BITMAP* img_small_fighter_broken2;
-static ALLEGRO_BITMAP* img_bullet;
+static ALLEGRO_BITMAP* img_middle_fighter;
+static ALLEGRO_BITMAP* img_plane_bullet;
+static ALLEGRO_BITMAP* img_middle_bullet;
 
 typedef struct {
     // The center coordinate of the image.
@@ -22,6 +25,8 @@ typedef struct {
     float w, h;
     // The velocity in x, y axes.
     float vx, vy;
+    // Rotation angle.
+    float theta;
     // Should we draw this object on the screen.
     bool hidden;
     // The pointer to the object’s image.
@@ -58,14 +63,20 @@ static void draw_movable_object(MovableObject obj, int pic_num);
 static void draw(void);
 static void destroy(void);
 
-#define MAX_ENEMY 5
-#define MAX_BULLET 50
+#define MAX_SMALL_ENEMY 5
+#define MAX_MIDDLE_ENEMY 3
+#define MAX_PLANE_BULLET 50
+#define MAX_MIDDLE_BULLET 6
 
 static Player plane;
-static Enemy enemies[MAX_ENEMY];
-static Bullet bullets[MAX_BULLET];
-static const float MAX_COOLDOWN = 0.1;
+static Enemy small_enemies[MAX_SMALL_ENEMY];
+static Enemy middle_enemies[MAX_MIDDLE_ENEMY];
+static Bullet plane_bullets[MAX_PLANE_BULLET];
+static Bullet middle_bullets[MAX_MIDDLE_BULLET];
+static const float MAX_COOLDOWN = 0.05;
 static double last_shoot_timestamp;
+static double last_middle_shoot_timestamp[MAX_MIDDLE_ENEMY];
+static double middle_bullet_max_turn_angle = 0.4 * PI / 60;
 static ALLEGRO_SAMPLE* bgm;
 static ALLEGRO_SAMPLE_ID bgm_id;
 static bool draw_gizmos;
@@ -76,19 +87,30 @@ static void init(void) {
     img_small_fighter = load_bitmap("resources\\smallfighter.png");
     img_small_fighter_broken1 = load_bitmap("resources\\smallfighter_broken1.png");
     img_small_fighter_broken2 = load_bitmap("resources\\smallfighter_broken2.png");
-    img_bullet = load_bitmap("resources\\image12.png");
+    img_middle_fighter = load_bitmap("resources\\rocket-3.png");
+    img_middle_bullet = load_bitmap("resources\\missile.png");
+    img_plane_bullet = load_bitmap("resources\\image12.png");
     img_plane = load_bitmap("resources\\plane.png");
 
 
     init_plane(&plane);
 
-    for (i = 0; i < MAX_ENEMY; i++) {
-        init_enemy(&enemies[i], 0);
+    for (i = 0; i < MAX_SMALL_ENEMY; i++) {
+        init_enemy(&small_enemies[i], 0);
     }
 
-    for (int i = 0; i < MAX_BULLET; i++) {
-        init_bullet(&bullets[i], 0);
+    for (int i = 0; i < MAX_MIDDLE_ENEMY; i++) {
+        init_enemy(&middle_enemies[i], 1);
     }
+
+    for (int i = 0; i < MAX_PLANE_BULLET; i++) {
+        init_bullet(&plane_bullets[i], 0);
+    }
+
+    for (int i = 0; i < MAX_MIDDLE_BULLET; i++) {
+        init_bullet(&middle_bullets[i], 1);
+    }
+
     // Can be moved to shared_init to decrease loading time.
     bgm = load_audio("resources\\mythica.ogg");
     game_log("Start scene initialized");
@@ -102,9 +124,9 @@ static void init_plane(Player* player) {
     player->obj.y = 500;
     player->obj.w = al_get_bitmap_width(player->obj.img[0]);
     player->obj.h = al_get_bitmap_height(player->obj.img[0]);
+    player->obj.theta = 0;
     player->max_health = 1;
     player->health = player->max_health;
-
 }
 
 
@@ -117,6 +139,11 @@ static void init_enemy(Enemy* enemy, int type) {
         enemy->obj.img[2] = img_small_fighter_broken2;
         enemy->max_health = 3;
         break;
+
+    case 1:
+        enemy->obj.img[0] = img_middle_fighter;
+        enemy->max_health = 1;
+        break;
     }
 
     enemy->type = type;
@@ -126,23 +153,31 @@ static void init_enemy(Enemy* enemy, int type) {
     enemy->obj.y = 80;
     enemy->obj.vx = 0;
     enemy->obj.vy = 1;
+    enemy->obj.theta = PI;
     enemy->obj.hidden = true;
     enemy->health = enemy->max_health;
 
 }
 
 static void init_bullet(Bullet* bullet, int type) {
-    //type 1: normal bullets, 2: mid fighter bullets
+    //type 1: plane bullets, 2: mid fighter bullets
     switch (type) {
     case 0:
-        bullet->obj.img[0] = img_bullet;
+        bullet->obj.img[0] = img_plane_bullet;
+        bullet->obj.vx = 0;
+        bullet->obj.vy = -20;
+        break;
+
+    case 1:
+        bullet->obj.img[0] = img_middle_bullet;
+        bullet->obj.vx = 0;
+        bullet->obj.vy = 0;
         break;
     }
     bullet->type = type;
-    bullet->obj.w = al_get_bitmap_width(img_bullet);
-    bullet->obj.h = al_get_bitmap_height(img_bullet);
-    bullet->obj.vx = 0;
-    bullet->obj.vy = -3;
+    bullet->obj.w = al_get_bitmap_width(bullet->obj.img[0]);
+    bullet->obj.h = al_get_bitmap_height(bullet->obj.img[0]);
+    bullet->obj.theta = 0;
     bullet->obj.hidden = true;
 }
 
@@ -159,9 +194,9 @@ int collision_detect(MovableObject obj1, MovableObject obj2) {
 }
 
 //restrict_in_LRUD: 0:check boundaries, 1: don't check boundaries, 2: go out then hidden
-void move_object(MovableObject* obj, float dx, float dy, int* restrict_in_LRUD) {
-    obj->x += dx;
-    obj->y += dy;
+void move_object(MovableObject* obj, int* restrict_in_LRUD) {
+    obj->x += obj->vx;
+    obj->y += obj->vy;
 
     if (restrict_in_LRUD[0] == 0) {
         if (obj->x - obj->w / 2 < 0)
@@ -212,14 +247,65 @@ void move_object(MovableObject* obj, float dx, float dy, int* restrict_in_LRUD) 
 
 
 static void update(void) {
-    for (int i = 0; i < MAX_ENEMY; i++) {
-        if (enemies[i].obj.hidden)
+    //collision detect
+    int i, j;
+    for (i = 0; i < MAX_SMALL_ENEMY; i++) {
+        if (small_enemies[i].obj.hidden)
             continue;
-        if (collision_detect(plane.obj, enemies[i].obj)) {
+        if (collision_detect(plane.obj, small_enemies[i].obj)) {
             game_change_scene(scene_menu_create());
         }
     }
+    for (i = 0; i < MAX_MIDDLE_ENEMY; i++) {
+        if (middle_enemies[i].obj.hidden)
+            continue;
+        if (collision_detect(plane.obj, middle_enemies[i].obj))
+            game_change_scene(scene_menu_create());
+    }
+    for (i = 0; i < MAX_MIDDLE_BULLET; i++) {
+        if (middle_bullets[i].obj.hidden)
+            continue;
+        if (collision_detect(plane.obj, middle_bullets[i].obj))
+            game_change_scene(scene_menu_create());
+    }
+    for (i = 0; i < MAX_PLANE_BULLET; i++) {
+        if (plane_bullets[i].obj.hidden)
+            continue;
+        for (j = 0; j < MAX_SMALL_ENEMY; j++) {
+            if (small_enemies[j].obj.hidden)
+                continue;
+            if (collision_detect(plane_bullets[i].obj, small_enemies[j].obj)) {
+                small_enemies[j].health--;
+                plane_bullets[i].obj.hidden = true;
+                if (small_enemies[j].health == 0)
+                    small_enemies[j].obj.hidden = true;
+            }
+        }
+        if (plane_bullets[i].obj.hidden)
+            continue;
+        for (j = 0; j < MAX_MIDDLE_ENEMY; j++) {
+            if (middle_enemies[j].obj.hidden)
+                continue;
+            if (collision_detect(plane_bullets[i].obj, middle_enemies[j].obj)) {
+                middle_enemies[j].health--;
+                plane_bullets[i].obj.hidden = true;
+                if (middle_enemies[j].health == 0)
+                    middle_enemies[j].obj.hidden = true;
+            }
+        }
+        if (plane_bullets[i].obj.hidden)
+            continue;
+        for (j = 0; j < MAX_MIDDLE_BULLET; j++) {
+            if (middle_bullets[j].obj.hidden)
+                continue;
+            if (collision_detect(plane_bullets[i].obj, middle_bullets[j].obj)) {
+                plane_bullets[i].obj.hidden = true;
+                middle_bullets[j].obj.hidden = true;
+            }
+        }
+    }
 
+    //create and move objects
     plane.obj.vx = plane.obj.vy = 0;
     if (key_state[ALLEGRO_KEY_UP] || key_state[ALLEGRO_KEY_W])
         plane.obj.vy -= 1;
@@ -229,77 +315,128 @@ static void update(void) {
         plane.obj.vx -= 1;
     if (key_state[ALLEGRO_KEY_RIGHT] || key_state[ALLEGRO_KEY_D])
         plane.obj.vx += 1;
-    move_object(&plane.obj, plane.obj.vx * 4 * (plane.obj.vy ? 0.71f : 1), plane.obj.vy * 4 * (plane.obj.vx ? 0.71f : 1), (int[]){0, 0, 0, 0});
 
+    plane.obj.vx *= 7 * (plane.obj.vy ? 0.71f : 1);
+    plane.obj.vy *= 7 * (plane.obj.vx ? 0.71f : 1);
+    move_object(&plane.obj, (int[]){0, 0, 0, 0});
 
-    int i, j;
-    for (i = 0; i < MAX_BULLET; i++) {
-        if (bullets[i].obj.hidden)
+    for (i = 0; i < MAX_PLANE_BULLET; i++) {
+        if (plane_bullets[i].obj.hidden)
             continue;
-        for (j = 0; j < MAX_ENEMY; j++) {
-            if (enemies[j].obj.hidden)
-                continue;
-            if (collision_detect(bullets[i].obj, enemies[j].obj)) {
-                enemies[j].health--;
-                bullets[i].obj.hidden = true;
-                if (enemies[j].health == 0)
-                    enemies[j].obj.hidden = true;
-            }
-        }
-    }
-
-    for (i = 0; i < MAX_BULLET; i++) {
-        if (bullets[i].obj.hidden)
-            continue;
-        move_object(&bullets[i].obj, bullets[i].obj.vx, bullets[i].obj.vy, (int[]) {0, 0, 2, 0});
+        move_object(&plane_bullets[i].obj, (int[]) {0, 0, 2, 0});
     }
 
     int hidden_enemy_count = 0;
     int enemy_group_size = 5;
     
-    for (i = 0; i < MAX_ENEMY; i++) {
-        if (enemies[i].obj.hidden) {
+    for (i = 0; i < MAX_SMALL_ENEMY; i++) {
+        if (small_enemies[i].obj.hidden) {
             hidden_enemy_count++;
             continue;
         }
-        enemies[i].obj.vx = -5*sin(enemies[i].obj.y/10);
-        move_object(&enemies[i].obj, enemies[i].obj.vx, enemies[i].obj.vy, (int[]) { 0, 0, 1, 2 });
+        small_enemies[i].obj.vx = -5*sin(small_enemies[i].obj.y/10);
+        move_object(&small_enemies[i].obj, (int[]) { 0, 0, 1, 2 });
+    }
+
+    for (i = 0; i < MAX_MIDDLE_ENEMY; i++) {
+        if (middle_enemies[i].obj.hidden && !(rand()%(FPS*MAX_MIDDLE_ENEMY))) {
+            middle_enemies[i].obj.hidden = false;
+            middle_enemies[i].obj.x = (rand() % 2) * SCREEN_W;
+            middle_enemies[i].obj.y = (rand() % 2) * SCREEN_H;
+            middle_enemies[i].health = middle_enemies[i].max_health;
+        }
+        middle_enemies[i].obj.theta = atan2((double)plane.obj.y - (double)middle_enemies[i].obj.y, (double)plane.obj.x - (double)middle_enemies[i].obj.x + 0.001);
+        if (middle_enemies[i].obj.theta < 0)
+            middle_enemies[i].obj.theta += 2 * PI;
+        middle_enemies[i].obj.vx = 2 * cos(middle_enemies[i].obj.theta);
+        middle_enemies[i].obj.vy = 2 * sin(middle_enemies[i].obj.theta);
+        move_object(&middle_enemies[i].obj, (int[]) { 0, 0, 0, 0 });
     }
 
     if (hidden_enemy_count >= enemy_group_size) {
-        float x_pos = enemies[0].obj.w / 2 + rand() % (int)(SCREEN_W - enemies[0].obj.w);
-        float y_pos = -enemies[0].obj.h/2;
-        for (i = 0, j = 0; i < MAX_ENEMY && j < enemy_group_size; i++) {
-            if (enemies[i].obj.hidden) {
-                enemies[i].obj.hidden = false;
-                enemies[i].obj.y = y_pos;
-                enemies[i].obj.x = x_pos + 50*cos(enemies[i].obj.y/10);
-                enemies[i].obj.vy = 1;
-                enemies[i].health = enemies[i].max_health;
-                y_pos -= enemies[i].obj.h + 5;
+        float x_pos = small_enemies[0].obj.w / 2 + rand() % (int)(SCREEN_W - small_enemies[0].obj.w);
+        float y_pos = -small_enemies[0].obj.h/2;
+        for (i = 0, j = 0; i < MAX_SMALL_ENEMY && j < enemy_group_size; i++) {
+            if (small_enemies[i].obj.hidden) {
+                small_enemies[i].obj.hidden = false;
+                small_enemies[i].obj.y = y_pos;
+                small_enemies[i].obj.x = x_pos + 50*cos(small_enemies[i].obj.y/10);
+                small_enemies[i].obj.vy = 1;
+                small_enemies[i].health = small_enemies[i].max_health;
+                y_pos -= small_enemies[i].obj.h + 5;
                 j++;
             }
         }
     }
 
+    for (i = 0; i < MAX_MIDDLE_BULLET; i++) {
+        if (middle_bullets[i].obj.hidden)
+            continue;
+
+        if (middle_bullets[i].obj.theta < 0)
+            middle_bullets[i].obj.theta += 2 * PI;
+        if (middle_bullets[i].obj.theta > 2 * PI)
+            middle_bullets[i].obj.theta = fmod(middle_bullets[i].obj.theta, 2 * PI);
+
+        double new_angle = atan2((double)plane.obj.y - (double)middle_bullets[i].obj.y, (double)plane.obj.x - (double)middle_bullets[i].obj.x + 0.00001);
+        if (new_angle < 0)
+            new_angle += 2 * PI;
+
+        double dangle;
+        if (fabs(new_angle - (double)middle_bullets[i].obj.theta) > PI)
+            dangle = 2 * PI - fabs(new_angle - middle_bullets[i].obj.theta);
+        else
+            dangle = fabs(new_angle - middle_bullets[i].obj.theta);
+
+        if (dangle > middle_bullet_max_turn_angle) {
+            int sign = ((fmod(new_angle - middle_bullets[i].obj.theta + 2 * PI, 2*PI) > 0 && fmod(new_angle - middle_bullets[i].obj.theta + 2 * PI, 2 * PI) < PI) ? 1 : -1);
+            middle_bullets[i].obj.theta += middle_bullet_max_turn_angle * sign;
+        }
+        else {
+            middle_bullets[i].obj.theta = new_angle;
+        }
+
+        middle_bullets[i].obj.vx = 3 * cos(middle_bullets[i].obj.theta);
+        middle_bullets[i].obj.vy = 3 * sin(middle_bullets[i].obj.theta);
+        move_object(&middle_bullets[i].obj, (int[]) { 2, 2, 2, 2 });
+    }
+
+    //shoot bullets
+
     double now = al_get_time();
     if (key_state[75] && now - last_shoot_timestamp >= MAX_COOLDOWN) {
-        for (i = 0; i < MAX_BULLET; i++) {
-            if (bullets[i].obj.hidden) {
+        for (i = 0; i < MAX_PLANE_BULLET; i++) {
+            if (plane_bullets[i].obj.hidden) {
                 last_shoot_timestamp = now;
-                bullets[i].obj.hidden = false;
-                bullets[i].obj.x = plane.obj.x;
-                bullets[i].obj.y = plane.obj.y;
+                plane_bullets[i].obj.hidden = false;
+                plane_bullets[i].obj.x = plane.obj.x;
+                plane_bullets[i].obj.y = plane.obj.y;
                 break;
             }
         }
     }
+
+    for (i = 0; i < MAX_MIDDLE_ENEMY; i++) {
+        if (!middle_enemies[i].obj.hidden && now - last_middle_shoot_timestamp[i] >= 5) {
+            for (j = 0; j < MAX_MIDDLE_BULLET; j++) {
+                if (middle_bullets[j].obj.hidden) {
+                    last_middle_shoot_timestamp[i] = now;
+                    middle_bullets[j].obj.hidden = false;
+                    middle_bullets[j].obj.x = middle_enemies[i].obj.x;
+                    middle_bullets[j].obj.y = middle_enemies[i].obj.y;
+                    middle_bullets[j].obj.theta = middle_enemies[i].obj.theta;
+                    break;
+                }
+            }
+        }
+    }
+
 }
 
 static void draw_movable_object(MovableObject obj, int pic_num) {
     if (obj.hidden)
         return;
-    al_draw_bitmap(obj.img[pic_num], round((double)obj.x - (double)obj.w / 2), round((double)obj.y - (double)obj.h / 2), 0);
+    al_draw_rotated_bitmap(obj.img[pic_num], obj.w/2, obj.h/2, obj.x, obj.y, obj.theta, 0);
     if (draw_gizmos) {
         al_draw_rectangle(round((double)obj.x - (double)obj.w / 2), round((double)obj.y - (double)obj.h / 2),
             round((double)obj.x + (double)obj.w / 2) + 1, round((double)obj.y + (double)obj.h / 2) + 1, al_map_rgb(255, 0, 0), 0);
@@ -309,11 +446,15 @@ static void draw_movable_object(MovableObject obj, int pic_num) {
 static void draw(void) {
     int i;
     al_draw_bitmap(img_background, 0, 0, 0);
-    for (i = 0; i < MAX_BULLET; i++)
-        draw_movable_object(bullets[i].obj, 0);
+    for (i = 0; i < MAX_PLANE_BULLET; i++)
+        draw_movable_object(plane_bullets[i].obj, 0);
     draw_movable_object(plane.obj, 0);
-    for (i = 0; i < MAX_ENEMY; i++)
-        draw_movable_object(enemies[i].obj, enemies[i].max_health- enemies[i].health);
+    for (i = 0; i < MAX_SMALL_ENEMY; i++)
+        draw_movable_object(small_enemies[i].obj, small_enemies[i].max_health- small_enemies[i].health);
+    for (i = 0; i < MAX_MIDDLE_ENEMY; i++)
+        draw_movable_object(middle_enemies[i].obj, 0);
+    for (i = 0; i < MAX_MIDDLE_BULLET; i++)
+        draw_movable_object(middle_bullets[i].obj, 0);
 }
 
 static void destroy(void) {
@@ -323,7 +464,7 @@ static void destroy(void) {
     al_destroy_bitmap(img_small_fighter_broken1);
     al_destroy_bitmap(img_small_fighter_broken2);
     al_destroy_sample(bgm);
-    al_destroy_bitmap(img_bullet);
+    al_destroy_bitmap(img_plane_bullet);
     stop_bgm(bgm_id);
     game_log("Start scene destroyed");
 }
